6197f3bb871e29401b015bf421afd5301c5c2b44
elnurruzmanov@yandex.ru <elnurruzmanov219autp@gmail.com>
2022-06-28 18:34:29 +0300

    Messages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+
+  const [messages, setMessages]= useState(0);
+
+  const [state, setState] = useState({
+    userName: "Rustam",
+    gender: "💻  Frontend Developer",
+  });
+
+  function addMessage(){
+    setMessages((next)=>next+1);
+  }
+  function deleteMessage(){
+    setMessages((next)=>next-1);
+  }
+
+  function changeContact(){
+    setState((next)=>{
+      return{
+        ...next,
+        userName:"Sarvar",
+        gender:"💻 Backend Developer"
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h1>
+          <i className="fa-solid fa-message message"></i> Messages:{messages}
+          <span className="alert-message">{messages}</span>
+        </h1>
+
+        <hr />
+
+        <h3>
+          <i className="fa-solid fa-user"></i>
+          {state.userName} - {state.gender}
+        </h3>
+        <div className="edit">
+          <i class="fa-solid fa-pen-to-square"></i>
+          <span onClick={changeContact}>edit</span>
+        </div>
+
+        <hr />
+
+        <button onClick={addMessage} className="btn1">
+          Add
+        </button>
+        <button onClick={deleteMessage} className="btn2">
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
